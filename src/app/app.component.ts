c95e97012b31a6f64fb66ad25c3f7d8ce7597c58
BHUVANESH\bhuvanesh <bhuvanesh.govindaraji@cesltd.com>
2017-09-02 12:52:31 +0530

    First commit for to-do list in angular 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
-
+export class Task {
+  title: string;
+  description: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  dashboard = 'ToDo List';
+  tasks: Task[] = [];
+  dashboardTasks: Task[] = [];
+  newTask = {};
+  tempTask = {};
+  showList = false;
+
+  onClick(task) {
+    this.tempTask = {
+      title: task.title,
+      description: task.description
+    };
+    this.showList = !this.showList;
+  }
+  onAdd(task) {
+    this.newTask = task;
+    this.tasks.push(task);
+    this.dashboardTasks.push(task);
+    this.newTask = {};
+  }
+
+  onSave(task) {
+      this.tasks.forEach((value, index) => {
+        this.dashboardTasks.forEach((value1, index1) => {
+          if (value === value1) {
+            this.tasks.splice(index, 1);
+            this.tasks.push(task);
+          }
+        });
+      });
+      this.showList = !this.showList;
+  }
+  onDelete(task) {
+    this.tasks.forEach((value, index) => {
+      this.dashboardTasks.forEach((value1, index1) => {
+        if (value === value1) {
+          this.tasks.splice(index, 1);
+        }
+      });
+    });
+    this.showList = !this.showList;
+}
+onCancel() {
+  this.showList = !this.showList;
+}
 }
